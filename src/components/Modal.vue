5270f3b9a378dabe5d29420ef2b20a383ecb1da6
Robin Hamill <hamillrobin@gmail.com>
2019-02-22 18:28:54 -0800

    New components

---
+++ src/components/Modal.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="skrim">
+    <div class="modal">
+      <header>
+        Disclaimer
+      </header>
+      <div class="content">
+        <p>This tool is made for reference only, and does not guarantee the accurance of any of the information or pricing provided.</p>
+        <p>Sign up links include affilate paramters.</p>
+
+        <v-button
+          @click.native="close"
+          type="square"
+        >
+          I accept
+        </v-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VButton from './VButton.vue';
+
+export default {
+  name: 'Modal',
+  components: {
+    VButton,
+  },
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+  },
+  mounted() {
+    document.addEventListener('keyup', (e) => {
+      e.keyCode === 27 ? this.close() : false;
+    });
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '../assets/styles/mixins'
+
+.skrim
+  position fixed
+  top 0
+  bottom 0
+  left 0
+  right 0
+  background rgba(0,0,0,0.5)
+  display flex
+  justify-content center
+  align-items center
+  z-index 20
+.modal
+  max-width 600px
+  width 90%
+  background #fff
+  shadow()
+
+header,
+.content
+  padding: 25px
+
+header
+  background var(--grey)
+
+</style>
---
+++ src/components/VButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <button :class="type">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  bame: 'VButton',
+  props: {
+    type: {
+      type: String,
+      default: 'square',
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '../assets/styles/mixins'
+
+button
+  border none
+  display flex
+  justify-content center
+  align-items center
+  line-height 1
+  background var(--grey)
+  transition(box-shadow)
+  &:hover,
+  &:active,
+  &:focus
+    shadow()
+
+.round
+  --size: 35px
+  width var(--size)
+  height var(--size)
+  border-radius 50%
+
+.square
+  padding 10px 20px
+  border-radius 5px
+
+</style>
