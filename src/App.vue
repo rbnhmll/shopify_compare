ee0a531f7b674ca732c4c3f89ff9a5ee80a75e27
Robin Hamill <hamillrobin@gmail.com>
2019-02-24 15:40:24 -0800

    Add vue head and meta

--- src/App.vue
+++ src/App.vue
@@ -43,6 +43,33 @@ export default {
   created() {
     this.$store.dispatch('getExchangeRates');
   },
+  head: {
+    title() {
+      return {
+        inner: 'Shopify Compare',
+        complement: 'Find the right pricing tier for your shop',
+      };
+    },
+    meta: [
+      { name: 'description', content: 'Find the best value pricing tier for your e-commerce store on Shopify or Etsy', id: 'desc' },
+      { name: 'keywords', content: 'Shopify, Shopify Pricing, Etsy, Etsy Pricing, Best value, Best deal, e-commerce, E-Commerce', id: 'keys' },
+      { name: 'author', content: 'Robin Hamill', id: 'author' },
+
+      { property: 'og:title', content: 'Shopify Compare | Find the right pricing tier for your shop', id: 'og-title' },
+      { property: 'og:site_name', content: 'Shopify Compare', id: 'og-site-name' },
+      { property: 'og:url', content: 'https://shopify-compare.club', id: 'og-url' },
+      { property: 'og:description', content: 'Find the best value pricing tier for your e-commerce store on Shopify or Etsy', id: 'og-description' },
+      { property: 'og:type', content: 'website', id: 'og-type' },
+      { property: 'og:image', content: 'https://shopify-compare.club/social.png', id: 'og-image' },
+
+      { property: 'twitter:site', content: 'https://shopify-compare.club', id: 'twitter-site' },
+      { property: 'twitter:card', content: 'summary_large_image', id: 'twitter-card' },
+      { property: 'twitter:creator', content: '@rbnhmll', id: 'twitter-creator' },
+      { property: 'twitter:title', content: 'Shopify Compare', id: 'twitter-title' },
+      { property: 'twitter:description', content: 'Find the best value pricing tier for your e-commerce store on Shopify or Etsy', id: 'twitter-description' },
+      { property: 'twitter:image', content: 'https://shopify-compare.club/social.png', id: 'twitter-image' },
+    ],
+  },
 };
 </script>
 
